cf02a3eca9f2a3e95ae6b488e983c53e2945fe50
Xxy8gxiydy8d8ydy8dxyixtixtixoyxy8x8ycyociycidyic8yxitxtixiyxyxyixyix <9696650@stud.kai.edu.ua>
2026-03-11 23:39:43 +0200

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,58 @@
+using LAB_3.TrapezoidLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LAB_3 {
+
+   
+
+    class Program
+    {
+        static void Main()
+        {
+            Trapezoid TR1 = new Trapezoid();
+            Trapezoid TR2 = new Trapezoid(5, 6, 3, 3, 4);
+            Trapezoid TR3 = new Trapezoid(TR2);
+
+            Console.WriteLine("TR1:");
+            Print(TR1);
+
+            Console.WriteLine("TR2:");
+            Print(TR2);
+
+            Console.WriteLine("TR3:");
+            Print(TR3);
+
+            
+            TR3--;
+
+            Console.WriteLine("TR3 -- :");
+            Print(TR3);
+
+            // множимо
+            TR1 = TR2 * TR3;
+
+            Console.WriteLine("TR1 = TR2 * TR3:");
+            Print(TR1);
+
+            Console.WriteLine("S TR1: " + TR1.Area());
+            Console.WriteLine("P TR1: " + TR1.Perimeter());
+        }
+
+        static void Print(Trapezoid t)
+        {
+            Console.WriteLine(
+                "a=" + t.A +
+                " b=" + t.B +
+                " c=" + t.C +
+                " d=" + t.D +
+                " h=" + t.H);
+        }
+    }
+
+
+}
